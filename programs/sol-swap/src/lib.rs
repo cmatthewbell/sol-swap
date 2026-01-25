116223b50e7ad7be0195aaa6d4f4da08c9e7ed2c
Gunhours <chrisbelldevelopment@gmail.com>
2026-01-25 16:42:33 -0500

    setup accounts for swapping from sol

--- programs/sol-swap/src/lib.rs
+++ programs/sol-swap/src/lib.rs
@@ -1,4 +1,5 @@
 use anchor_lang::prelude::*;
+use anchor_spl::token::TokenAccount;
 
 declare_id!("2cESwGJN1TtkYENEYqQFJNAjDnkyhHjCUUeRmibP8RuP");
 
@@ -6,12 +7,31 @@ declare_id!("2cESwGJN1TtkYENEYqQFJNAjDnkyhHjCUUeRmibP8RuP");
 pub mod sol_swap {
     use super::*;
 
-    pub fn create_swap() {
-
+    // creates swap with SOL and wants SPL
+    pub fn create_swap_from_sol(ctx: Context<CreateSwapFromSol>, offered_amount: u64, wanted_asset: Asset) -> Result<()> {
+        // transfer SOL from maker to escrow
+        // Populate escrow account with account fields
     }
+
+    // creates swap with SPL and wants SOL
+}
+
+#[derive(Accounts)]
+pub struct CreateSwapFromSol<'info> {
+    #[account(mut)]
+    pub maker: Signer<'info>,
+    #[account(init,
+        payer = maker,
+        space = 8 + Escrow::INIT_SPACE,
+        seeds = [b"escrow", maker.key().as_ref()],
+        bump,
+    )]
+    pub escrow: Account<'info, Escrow>,
+    pub system_program: Program<'info, System>,
 }
 
 #[account]
+#[derive(InitSpace)]
 pub struct Escrow {
     pub maker: Pubkey,
     pub offered_asset: Asset,
@@ -20,6 +40,7 @@ pub struct Escrow {
 }
 
 #[derive(AnchorSerialize, AnchorDeserialize, Clone)]
+#[derive(InitSpace)]
 pub enum Asset {
     Sol {
         amount: u64,
